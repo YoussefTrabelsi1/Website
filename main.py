1a2fe589d4e6674796259baf1b0c032298bb79c0
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2024-11-23 11:39:23 +0100

    Fixing bug where sidebar doesnt open on its own in mobile layout

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import streamlit as st
 from my_app.pages.main_page import render_main_page
 
 def main():
-    
+    st.set_page_config(
+    page_title="My Portfolio",
+    page_icon="🌟",
+    layout="centered",
+    initial_sidebar_state="expanded"  # Sidebar starts open
+)
 
     # Navigation menu for different pages
     render_main_page()
