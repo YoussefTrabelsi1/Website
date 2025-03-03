609be499c0fa62d9af8546ce8fdbf2e75014e554
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-03-03 23:51:45 +0100

    contact

--- my_app/widgets/contact.py
+++ my_app/widgets/contact.py
@@ -3,7 +3,6 @@ import streamlit as st
 def render_contact():
     
     st.markdown('<h1 style="color: #3395c4;">Contact Me</h1>', unsafe_allow_html=True)
-    
     # Displaying contact information
     st.subheader("You can reach me at:")
     
