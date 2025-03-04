f2969c735828dc1f676589160efcd3ccfcbbe414
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-03-04 22:34:30 +0100

    contact

--- my_app/widgets/contact.py
+++ my_app/widgets/contact.py
@@ -5,7 +5,6 @@ def render_contact():
     st.markdown('<h1 style="color: #3395c4;">Contact Me</h1>', unsafe_allow_html=True)
     # Displaying contact information
     st.subheader("You can reach me at:")
-    
     # Displaying email and LinkedIn contact
     st.write("Email : yousseftrabelsi250@gmail.com")
     st.write("LinkedIn : [Youssef Trabelsi](https://www.linkedin.com/in/youssef-trabelsi-b57b53263/)")
